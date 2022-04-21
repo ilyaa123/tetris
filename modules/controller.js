e03231f8753797a8dfba1bcacf77e83c4fd940e3
Ilya Andreev <ilya_a2018@mail.ru>
2022-04-21 22:24:57 +0300

    tetris

---
+++ modules/controller.js
@@ -0,0 +1,44 @@
+export class Controller{
+    constructor(game, view){
+        this.game = game;
+        this.view = view;
+    }
+    init(codeKey){
+        window.addEventListener('keydown', event => {
+            if (event.code === codeKey){
+                this.view.init();
+                this.start();
+            }
+        });
+    }
+    start(){
+        this.view.showArea(this.game.vieArea);
+
+        setInterval(() => {
+            this.game.moveDown();
+            this.view.showArea(this.game.vieArea)
+        }, 600);
+
+        window.addEventListener('keydown', (event) => {
+            const key = event.code;
+            switch (key) {
+                case 'ArrowLeft':
+                    this.game.moveLeft();
+                    this.view.showArea(this.game.vieArea)
+                    break;
+                case 'ArrowRight':
+                    this.game.moveRight();
+                    this.view.showArea(this.game.vieArea)
+                    break;
+                case 'ArrowDown':
+                    this.game.moveDown();
+                    this.view.showArea(this.game.vieArea)
+                    break;
+                case 'ArrowUp':
+                    this.game.rotateTetromino();
+                    this.view.showArea(this.game.vieArea)
+                    break;
+            }
+        });
+    }
+}
